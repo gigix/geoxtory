701d7dcdfd28b40ddbdfd40b42ed51cd3f9db02b
Jeff Xiong <gigix1980@gmail.com>
2012-08-19 22:06:19 -0400

    spike: plot locations and descriptions on google map

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoxtory::Application.config.secret_token = '971ca51c852ab067ab9658024b6be1f1bbb5d763a8a19cf1258628e97f5b460a2ed256222615708a374a4ae6513985a72291b94ec1ef320d072f9a7a6d373d13'
