779497ccd9429c0b9b89d08af35a5ffc70a0c061
cuipengfei <cuipengfei2008@gmail.com>
2012-10-16 18:54:45 +0800

    changed deployment server name

--- DeployToHeroku.sh
+++ DeployToHeroku.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "Tip: Please make sure you have commited your latest code before you deploy to heroku."
+echo "Before using this script, you need to install heroku client in your dev machine."
+echo "and then you need to login with our shared accout(ask Pengfei for its)"
 git push heroku master
 
 echo "reset heroku database"
-heroku pg:reset DATABASE --confirm powerful-shore-6235
+heroku pg:reset DATABASE --confirm geoxtorydev
 
 echo "migrate heroku database"
 heroku run rake db:migrate
