18760ec36b2f81fca504adad00729e6ab5055b8f
Bowen <huangbowen521@gmail.com>
2012-10-31 22:39:25 +0800

    clean some bad smell

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ActiveRecord::Base
+  attr_accessible :trip_id, :name, :description, :longitude, :latitude, :link_url, :image_url, :thumbnail_url
   def to_csv
     # Name,Description,Icon_color,Long,Lat,URL,Thumb_URL
     %Q("#{name}","#{description.gsub('"', '""')}",R,"#{longitude}","#{latitude}","#{image_url}","#{thumbnail_url}")
