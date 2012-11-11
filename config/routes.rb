876accf46483ecd739e38d44b549686f889ed4b3
Bowen <huangbowen521@gmail.com>
2012-11-11 21:00:56 +0800

    adjust the sharing code position.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Geoxtory::Application.routes.draw do
     end
     resources :locations, :only => [:create, :destroy]
   end
+
+  root :to => "welcome#index"
+
   # Te priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -57,7 +60,6 @@ Geoxtory::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
