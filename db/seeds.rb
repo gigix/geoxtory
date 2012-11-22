d7e252a50a649f9222fc851f12dea4ce74157b99
Bowen <huangbowen521@gmail.com>
2012-11-22 22:28:32 +0800

    Correct seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,26 +11,15 @@
 
 puts "creating seed data......"
 
-locations = Location.create({:name => "chengdu1",
+locations = Location.create(:name => "chengdu1",
                              :description => "logo of rails",
                              :latitude => "30.657419659093495",
                              :longitude => "104.06695885186764",
                              :image_url => "http://img3.douban.com/lpic/s9102133.jpg",
-                             :thumbnail_url => "http://img3.douban.com/lpic/s9102133.jpg"},
-                            {:name => "chengdu2",
-                             :description => "logo of rails",
-                             :latitude => "30.657419659093495",
-                             :longitude => "104.06695885186764",
-                             :image_url => "http://img3.douban.com/lpic/s9102133.jpg",
-                             :thumbnail_url => "http://img3.douban.com/lpic/s9102133.jpg"},
-                            {:name => "chengdu3",
-                             :description => "logo of rails",
-                             :latitude => "30.657419659093495",
-                             :longitude => "104.06695885186764",
-                             :image_url => "http://img3.douban.com/lpic/s9102133.jpg",
-                             :thumbnail_url => "http://img3.douban.com/lpic/s9102133.jpg"})
-                            trip1 = Trip.create(:name => "item1",
-                                                :description => "test trip1",
-                                                :locations => [locations])
+                             :thumbnail_url => "http://img3.douban.com/lpic/s9102133.jpg"
+                            )
+trip1 = Trip.create(:name => "item1",
+                    :description => "test trip1",
+                    :locations => [locations])
 
 puts "seed data created."
