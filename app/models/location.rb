9b1b50e2a240428c4b3c87cb026e424ce2f574a8
Bowen <huangbowen521@gmail.com>
2012-11-24 16:17:11 +0800

    create seed data.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ActiveRecord::Base
+  belongs_to :trip
   attr_accessible :trip_id, :name, :description, :longitude, :latitude, :link_url, :image_url, :thumbnail_url
   def to_csv
     # Name,Description,Icon_color,Long,Lat,URL,Thumb_URL
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,55 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-# Location.delete_all
-# Trip.delete_all
+Location.delete_all
+Trip.delete_all
 
 puts "creating seed data......"
 
-locations = Location.create(:name => "chengdu1",
-                             :description => "logo of rails",
-                             :latitude => "30.657419659093495",
-                             :longitude => "104.06695885186764",
-                             :image_url => "http://img3.douban.com/lpic/s9102133.jpg",
-                             :thumbnail_url => "http://img3.douban.com/lpic/s9102133.jpg"
-                            )
-trip1 = Trip.create(:name => "item1",
-                    :description => "test trip1",
-                    :locations => [locations])
+trip = Trip.create(name: "my first trip",
+                   description: "trips on away day"
+)
+
+trip.locations.create(name: "chengdu1",
+                      description: "logo of rails",
+                      latitude: "30.657419659093495",
+                      longitude: "104.06695885186764",
+                      image_url: "DSC01400.JPG",
+                      thumbnail_url: "DSC01400.JPG")
+
+trip.locations.create(name: "chengdu2",
+                      description: "logo of rails",
+                      latitude: "30.657419659093495",
+                      longitude: "104.06695885186764",
+                      image_url: "DSC01401.JPG",
+                      thumbnail_url: "DSC01401.JPG")
+
+trip.locations.create(name: "chengdu3",
+                      description: "logo of rails",
+                      latitude: "30.657419659093495",
+                      longitude: "104.06695885186764",
+                      image_url: "DSC01442.JPG",
+                      thumbnail_url: "DSC01442.JPG")
+
+trip.locations.create(name: "chengdu4",
+                      description: "logo of rails",
+                      latitude: "30.657419659093495",
+                      longitude: "104.06695885186764",
+                      image_url: "DSC01446.JPG",
+                      thumbnail_url: "DSC01446.JPG")
+
+trip.locations.create(name: "chengdu5",
+                      description: "logo of rails",
+                      latitude: "30.657419659093495",
+                      longitude: "104.06695885186764",
+                      image_url: "DSC01401.JPG",
+                      thumbnail_url: "DSC01401.JPG")
+
+trip.locations.create(name: "chengdu6",
+                      description: "logo of rails",
+                      latitude: "30.657419659093495",
+                      longitude: "104.06695885186764",
+                      image_url: "DSC01401.JPG",
+                      thumbnail_url: "DSC01401.JPG")
 
 puts "seed data created."
